8ce5ad85a619abdf0f32dc44549d8945aa223d7d
Vishnu Penthalavishnu <vishnupenthala98@gmail.com>
2024-10-19 09:18:55 +0530

    first commit

---
+++ math-notes/cal-frontend/postcss.config.cjs
@@ -0,0 +1,14 @@
+module.exports={
+    plugins: {
+        'postess-preset-mantine': 1],
+        'postcss-simple-vars' : {
+            variables: {
+            'mantine-breakpoint-xs': '36em'
+            'mantine-breakpoint-sm': '48em'
+            'mantine-breakpoint-d': ' 62em'
+            'mantine-treakpoint-lg': '75em'
+            'mantine-breakpoint-xl': ' 88em'
+            },
+        },
+    },  
+};
